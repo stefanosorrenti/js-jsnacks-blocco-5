ed90fa96e61e592dbcce955858d00afb62019572
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-20 13:58:27 +0100

    add 3th snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,24 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//Usando il MAP
+const numbersUp = numbers.map(numb => {
+    numb++
+    return numb
+    
+})
+
+console.log(numbersUp);
+
+
+//USANDO IL FOR EACH
+
+const numbEach = []
+
+numbers.forEach(numb => {
+    numb++
+    numbEach.push(numb)
+});
+
+console.log(numbEach);
