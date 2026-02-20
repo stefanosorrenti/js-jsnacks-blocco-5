6446f9667e6a5973013a0c15ae7f1460d1811b96
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-20 13:50:46 +0100

    add 2th script

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,28 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+//USANDO IL CICLO EACH
+//RETTIFICO
+//Dato che il codice ci sta su una riga posso usare l'arrow function contratta
+// Uso la versione contratta quindi: 
+// Inserisco =>, posso evitare di mettere le tondo 
+// perchè abbiamo solo un parametro e dato che ci sta in un riga 
+// posso evitare di scrivere le graffe.
+const peopleName = []
+
+
+people.forEach(person => peopleName.push(person.name));
+console.log(peopleName);
+
+
+
+//USANDO IL MAP
+const nameMap = people.map(person => {
+  return person.name
+  
+})
+
+console.log(nameMap);
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
